f64f8160c48136151c6068a96818ebe543231588
aristidesquetajr <aristidesquetajr@gmail.com>
2023-05-02 06:59:00 +0000

    Added middleware for check exists user in request

--- src/index.js
+++ src/index.js
@@ -11,7 +11,17 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(u => u.username === username);
+
+  if(!user) {
+    return response.status(404).json({ error: "User not found" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
