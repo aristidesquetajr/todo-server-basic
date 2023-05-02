9f6d1af34174c5211bf36cb3821ba23a31b365ff
aristidesquetajr <aristidesquetajr@gmail.com>
2023-05-02 07:29:52 +0000

    Added route for edit to-do

--- src/index.js
+++ src/index.js
@@ -69,7 +69,22 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { id } = request.params;
+  const { user } = request;
+  
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo) {
+    return response.status(404).json({ error: "Todo not found" });
+  }
+
+  const todoIndex = user.todos.indexOf(todo);
+
+  user.todos[todoIndex].title = title;
+  user.todos[todoIndex].deadline = deadline;
+  
+  return response.json(user.todos[todoIndex]);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
